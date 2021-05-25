f2c3bd99fdfdda8ebf0731c7b7a7337802def567
Swapnil Chand <swapnilchand51@gmail.com>
2021-05-25 16:49:36 +0530

    exit features

--- app/src/main/java/com/example/think/MainActivity.java
+++ app/src/main/java/com/example/think/MainActivity.java
@@ -3,17 +3,20 @@ package com.example.think;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.ExifInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton startBtn;
+    private ImageButton exitBtn = findViewById(R.id.exit);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         startBtn = findViewById(R.id.start);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +25,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(q1Intent);
             }
         });
+        exitBtn = findViewById(R.id.exit);
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+
+
     }
 }
