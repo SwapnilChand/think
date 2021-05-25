b24abca19c9fd65ad4ed6875fdce878cf655de95
Swapnil Chand <swapnilchand51@gmail.com>
2021-05-25 16:53:25 +0530

    exit features

--- app/src/main/java/com/example/think/MainActivity.java
+++ app/src/main/java/com/example/think/MainActivity.java
@@ -3,35 +3,24 @@ package com.example.think;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.ExifInterface;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-    private ImageButton startBtn;
-    private ImageButton exitBtn = findViewById(R.id.exit);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startBtn = findViewById(R.id.start);
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent q1Intent = new Intent(MainActivity.this, QuestionsActivity.class);
-                startActivity(q1Intent);
-            }
+        ImageButton  startBtn = findViewById(R.id.start);
+        startBtn.setOnClickListener(v -> {
+            Intent q1Intent = new Intent(MainActivity.this, QuestionsActivity.class);
+            startActivity(q1Intent);
         });
-        exitBtn = findViewById(R.id.exit);
-        exitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.exit(0);
-            }
-        });
+        ImageButton exitBtn = findViewById(R.id.exit);
+        exitBtn.setOnClickListener(v -> System.exit(0));
 
 
     }
